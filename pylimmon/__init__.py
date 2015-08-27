86855639066c971d88983a392c0dd813c293bdbc
matthewdahmer <matthew.dahmer@gmail.com>
2015-08-27 12:42:28 -0400

    Fix name of imported function, 'create_db' in __init__.py

--- pylimmon/__init__.py
+++ pylimmon/__init__.py
@@ -1,4 +1,4 @@
-from glimmondb import get_tdb, read_glimmon, createdb, recreate_db, merge_new_glimmon_to_db
+from glimmondb import get_tdb, read_glimmon, create_db, recreate_db, merge_new_glimmon_to_db
 from pylimmon import open_sqlite_file, open_tdb_file, get_tdb_limits, get_safety_limits
 from pylimmon import check_limit_msid, check_state_msid, get_limits, get_states, get_mission_safety_limits
 from .version import __version__
